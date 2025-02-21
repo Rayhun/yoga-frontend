e39b5e9e24c8609c0b93ff6ad148809d2e1552d8
Muhammad Ibtesam Arshad <miap8380@gmail.com>
2025-02-21 09:51:25 +0500

    added payment status pages

---
+++ src/app/subscription/(status)/success/page.jsx
@@ -0,0 +1,11 @@
+import SubscriptionStatus from '@/components/subscription/common/SubscriptionStatus';
+
+export const metadata = {
+  title: 'Subscription Success',
+};
+
+const Page = () => {
+  return <SubscriptionStatus variant="success" />;
+};
+
+export default Page;
