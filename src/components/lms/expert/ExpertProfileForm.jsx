4a617d0967bc29a3ce6e95caa655d8a71594eca0
abrehman90 <meharabrehman@gmail.com>
2025-03-20 02:39:25 +0500

    required to optional program file

--- src/components/lms/expert/ExpertProfileForm.jsx
+++ src/components/lms/expert/ExpertProfileForm.jsx
@@ -189,7 +189,6 @@ const ExpertProfileForm = ({ selected }) => {
                       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
                       'application/vnd.ms-excel': ['.xls'],
                     }}
-                    required
                   />
                 </div>
               </div>
