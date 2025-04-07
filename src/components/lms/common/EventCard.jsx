ab9843d9f12f8db53b654bb9af06cb7f93ca5cf6
Anas Saleem <anaspucit2015@gmail.com>
2025-04-07 20:49:29 +0500

    feature: expert profile program and event management flow added

---
+++ src/components/lms/common/EventCard.jsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Image from 'next/image';
+import { GoDotFill } from 'react-icons/go';
+
+const EventCard = ({ event, onClick }) => {
+  return (
+    <div
+      className="rounded-lg border border-stroke bg-white shadow-default cursor-pointer overflow-hidden dark:bg-boxdark"
+      onClick={onClick}
+    >
+      <div className="aspect-[16/9]">
+        <Image
+          width={200}
+          height={200}
+          src={event.image}
+          alt="event-image"
+          className="w-full h-full object-cover rounded-t-lg"
+        />
+      </div>
+
+      <div className="p-4 flex flex-col gap-1">
+        <h4 className="text-lg font-semibold block truncate text-black dark:text-white">
+          {event.title}
+        </h4>
+
+        <div className="text-sm text-gray-500">
+          <span className="font-medium">Organizer:</span> {event.organizer}
+        </div>
+
+        <div className="text-sm text-gray-400 truncate" title={event.agenda}>
+          {event.agenda}
+        </div>
+
+        <div className="flex gap-1 items-center text-sm text-gray-500">
+          <p>{event.date}</p>
+          <GoDotFill size={8} />
+          <p>{event.time}</p>
+          <GoDotFill size={8} />
+          <p>{event.duration}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
