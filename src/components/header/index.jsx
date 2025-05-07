f8666cb24ad365bc40345d41aa786f4fa402359d
Abdul Rehman <79878896+abrehman90@users.noreply.github.com>
2025-05-08 03:52:08 +0500

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -54,9 +54,9 @@ const Header = props => {
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
 
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
+{/*           <Link className="block flex-shrink-0 lg:hidden" href="/">
             <Image width={32} height={32} src={'/images/logo/logo-icon.svg'} alt="Logo" />
-          </Link>
+          </Link> */}
         </div>
 
         <div className="flex items-center self-end gap-3 2xsm:gap-7">
