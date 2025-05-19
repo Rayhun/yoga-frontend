841f3610c3b96acf8aa8e805f6cdc57d2e6080b6
Anas Saleem <anaspucit2015@gmail.com>
2025-05-20 01:59:36 +0500

    fix: limit enabled while adding multiple certifications

--- src/components/lms/expert/ExpertProfileForm.jsx
+++ src/components/lms/expert/ExpertProfileForm.jsx
@@ -69,7 +69,8 @@ const ExpertProfileForm = ({ selected }) => {
     languages: Yup.array().of(Yup.string().required('Required!')).min(1, 'At least 1 language is required'),
     credentials: Yup.array()
       .of(Yup.string().required('Required!'))
-      .min(1, 'At least 1 credential is required'),
+      .min(1, 'At least 1 credential is required')
+      .max(5, 'Maximum 5 credentials are allowed'),
     // coaching_content: Yup.array()
     //   .of(Yup.string().required('Required!'))
     //   .min(1, 'At least 1 coaching content is required'),
@@ -187,7 +188,7 @@ const ExpertProfileForm = ({ selected }) => {
               <FormikSubmittableField
                 name="credentials"
                 label="Certifications"
-                placeholder="Your Certifications Link or URL"
+                placeholder="Add a certification (e.g. Health Coach). Press return to confirm, max 5."
                 required
               />
               <FormikMultiSelect
