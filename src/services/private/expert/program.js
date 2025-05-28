60268726b2e95138294842222123e434c45ff680
Abdul Rehman <79878896+abrehman90@users.noreply.github.com>
2025-05-28 21:58:39 +0500

    Update program.js

--- src/services/private/expert/program.js
+++ src/services/private/expert/program.js
@@ -7,9 +7,10 @@ export const getExpertProgramsList = async () => {
 };
 
 export const uploadPrograms = async ({ payload }) => {
+  console.log('payload', payload)
   const formData = new FormData();
-  Object.entries(payload).forEach(([key, value]) => {
-    if (key==='available' || value) formData.set(key, value);
+  payload.program_file.forEach((file) => {
+    formData.append("program_file", file);
 
   });
   // return axios.post(`LMS/experts/upload/programs/`, payload);
